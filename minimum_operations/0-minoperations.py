92c00e32f38a71a384c169f94610b4ca46271816
uwituzeb <b.uwituze@alustudent.com>
2024-01-19 10:13:03 +0200

    min operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""minimum operations"""
+import math
+
+def minOperations(n):
+    """minimum operations to get n to 1"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % 1 == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+
+    return operations
